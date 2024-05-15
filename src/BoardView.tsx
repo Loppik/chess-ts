360ce7c420d7df552d9668b403dc44a5332736cf
alexandershantyr <shantyrqq@gmail.com>
2024-05-15 17:31:07 +0300

    refactor(game): move check on correct move into game class

--- src/BoardView.tsx
+++ src/BoardView.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { styled } from 'styled-components';
 import BoardBorders from './BoardBorders';
-import { CELL_SIZE, FigureColor, FigureType } from './constants';
+import { CELL_SIZE } from './constants';
 import { IStyledComponentProps } from './interfaces';
 import { convertBoardLayoutToPosition } from './helpers';
 import { TCellPosition, TCellPositionStrict, TFigure } from './types';
@@ -105,60 +105,10 @@ const BoardView = () => {
       return;
     }
 
-    if (
-      checkIsCorrectMove(
-        game.getCell(firstSelectedPosition)!,
-        firstSelectedPosition,
-        cellPosition,
-      )
-    ) {
-      game.moveFigure(firstSelectedPosition, cellPosition);
+    const isMoved = game.moveFigure(firstSelectedPosition, cellPosition);
+    if (isMoved) {
       setFirstSelectedPosition(null);
       game.endMove();
-    }
-  };
-
-  const checkIsCorrectMove = (
-    figure: TFigure,
-    fromPosition: TCellPosition,
-    toPosition: TCellPosition,
-  ) => {
-    if (!fromPosition || !toPosition) {
-      return false;
-    }
-    if (figure.type === FigureType.Pawn) {
-      if (figure.color === FigureColor.White) {
-        const defaultMove =
-          toPosition.posX === fromPosition.posX &&
-          toPosition.posY === fromPosition.posY + 1;
-
-        if (fromPosition.posY === 1) {
-          return (
-            defaultMove ||
-            (toPosition.posX === fromPosition.posX &&
-              toPosition.posY === fromPosition.posY + 2)
-          );
-        }
-        return defaultMove;
-      } else {
-        const defaultMove =
-          toPosition.posX === fromPosition.posX &&
-          toPosition.posY === fromPosition.posY - 1;
-
-        if (fromPosition.posY === 6) {
-          return (
-            defaultMove ||
-            (toPosition.posX === fromPosition.posX &&
-              toPosition.posY === fromPosition.posY - 2)
-          );
-        }
-        return defaultMove;
-      }
-    } else if (figure.type === FigureType.Rook) {
-      return (
-        toPosition.posX === fromPosition.posX ||
-        toPosition.posY === fromPosition.posY
-      );
     }
   };
 
--- src/ChessGame.ts
+++ src/ChessGame.ts
@@ -1,6 +1,6 @@
 import { convertPositionToBoardLayout, generateInitialBoard } from './helpers';
-import { FigureColor } from './constants';
-import { TBoard, TCellPosition, TCellPositionStrict } from './types';
+import { FigureColor, FigureType } from './constants';
+import { TBoard, TCellPosition, TCellPositionStrict, TFigure } from './types';
 
 class ChessGame {
   public board: TBoard;
@@ -16,9 +16,19 @@ class ChessGame {
     return this.board[row][col];
   }
 
-  moveFigure(fromPosition: TCellPosition, toPosition: TCellPosition) {
+  moveFigure(fromPosition: TCellPosition, toPosition: TCellPosition): boolean {
     if (!fromPosition || !toPosition) {
-      return;
+      return false;
+    }
+
+    if (
+      !this.checkIsCorrectMove(
+        this.getCell(fromPosition)!,
+        fromPosition,
+        toPosition,
+      )
+    ) {
+      return false;
     }
 
     const toPositionLayout = convertPositionToBoardLayout(toPosition);
@@ -26,6 +36,48 @@ class ChessGame {
     this.board[toPositionLayout.row][toPositionLayout.col] =
       this.board[fromPositionLayout.row][fromPositionLayout.col];
     this.board[fromPositionLayout.row][fromPositionLayout.col] = null;
+    return true;
+  }
+
+  checkIsCorrectMove(
+    figure: TFigure,
+    fromPosition: TCellPositionStrict,
+    toPosition: TCellPositionStrict,
+  ) {
+    if (figure.type === FigureType.Pawn) {
+      if (figure.color === FigureColor.White) {
+        const defaultMove =
+          toPosition.posX === fromPosition.posX &&
+          toPosition.posY === fromPosition.posY + 1;
+
+        if (fromPosition.posY === 1) {
+          return (
+            defaultMove ||
+            (toPosition.posX === fromPosition.posX &&
+              toPosition.posY === fromPosition.posY + 2)
+          );
+        }
+        return defaultMove;
+      } else {
+        const defaultMove =
+          toPosition.posX === fromPosition.posX &&
+          toPosition.posY === fromPosition.posY - 1;
+
+        if (fromPosition.posY === 6) {
+          return (
+            defaultMove ||
+            (toPosition.posX === fromPosition.posX &&
+              toPosition.posY === fromPosition.posY - 2)
+          );
+        }
+        return defaultMove;
+      }
+    } else if (figure.type === FigureType.Rook) {
+      return (
+        toPosition.posX === fromPosition.posX ||
+        toPosition.posY === fromPosition.posY
+      );
+    }
   }
 
   endMove() {
